f6fe35f023e0bc99bc88879e8896e71648af0672
sueszli <jabaryyahya@gmail.com>
2025-12-24 15:53:05 +0100

    up

--- aziz-lang-tiny/main.py
+++ aziz-lang-tiny/main.py
@@ -8,8 +8,6 @@
 # ///
 
 import argparse
-import subprocess
-import tempfile
 from parser import AzizParser
 from pathlib import Path
 
@@ -23,22 +21,24 @@ from xdsl.dialects.builtin import Builtin
 from xdsl.transforms.canonicalize import CanonicalizePass
 from xdsl.transforms.lower_affine import LowerAffinePass
 
+# usage:
+#
+# $ uv run aziz-lang-tiny/main.py examples/rec.aziz --debug
+# $ uv run aziz-lang-tiny/main.py examples/rec.aziz | mlir-opt --convert-scf-to-cf --convert-func-to-llvm --convert-arith-to-llvm --convert-cf-to-llvm --reconcile-unrealized-casts | mlir-translate --mlir-to-llvmir | lli
+
 
 def main():
     parser = argparse.ArgumentParser(description="aziz language")
     parser.add_argument("file", help="source file")
-    parser.add_argument("--mlir", action="store_true", help="emit intermediate mlir representations")
+    parser.add_argument("--debug", action="store_true", help="emit intermediate mlir representations")
     args = parser.parse_args()
     assert args.file.endswith(".aziz")
     src = Path(args.file).read_text()
 
-    # aziz dialect -> llvm dialect mlir -> llvm ir -> printed, then piped into bash script
-    # uv run aziz-lang-tiny/main.py examples/rec.aziz | ...
-
     module_ast = AzizParser(None, src).parse_module()
     module_op = IRGen().ir_gen_module(module_ast)
 
-    if args.mlir:
+    if args.debug:
         print(f"\n\n{'-' * 30} MLIR before optimization\n\n")
         print(module_op)
 
@@ -46,7 +46,7 @@ def main():
     OptimizeAzizPass().apply(ctx, module_op)
     module_op.verify()
 
-    if args.mlir:
+    if args.debug:
         print(f"\n\n{'-' * 30} MLIR after optimization\n\n")
         print(module_op)
 
@@ -56,16 +56,9 @@ def main():
     LowerPrintfToLLVMCallPass().apply(ctx, module_op)
     module_op.verify()
 
-    if args.mlir:
+    if args.debug:
         print(f"\n\n{'-' * 30} MLIR after lowering to LLVM dialect\n\n")
     print(module_op)
-
-    # llvm_ir, llvm_exec_out, llvm_exec_err = execute_llvm(module_op)
-    # print(llvm_ir)
-
-    # print(f"{'-' * 30} LLVM output")
-    # print(llvm_exec_out)
-    # print(llvm_exec_err)
 
 
 def context() -> Context:
@@ -78,33 +71,6 @@ def context() -> Context:
     ctx.load_dialect(printf.Printf)
     ctx.load_dialect(scf.Scf)
     return ctx
-
-
-def execute_llvm(module_op_llvm):
-    res = subprocess.run(["mlir-opt", "--convert-scf-to-cf", "--convert-func-to-llvm", "--convert-arith-to-llvm", "--convert-cf-to-llvm", "--reconcile-unrealized-casts"], input=str(module_op_llvm), capture_output=True, text=True)
-    assert res.returncode == 0, f"mlir-opt failed:\n{res.stderr}"
-    mlir_opt = res.stdout
-
-    res = subprocess.run(["mlir-translate", "--mlir-to-llvmir"], input=mlir_opt, capture_output=True, text=True)
-    assert res.returncode == 0, f"mlir-translate failed:\n{res.stderr}"
-    llvm_ir = res.stdout
-
-    with tempfile.NamedTemporaryFile(mode="w", suffix=".ll", delete=False) as f:
-        f.write(llvm_ir)
-        ll_path = f.name
-
-    obj_path = ll_path.replace(".ll", ".o")
-    res = subprocess.run(["llc", "-filetype=obj", "-o", obj_path, ll_path], capture_output=True, text=True)
-    assert res.returncode == 0, f"llc failed:\n{res.stderr}"
-    with tempfile.NamedTemporaryFile(delete=False) as f:
-        output_file = f.name
-    res = subprocess.run(["clang", "-o", output_file, obj_path], capture_output=True, text=True)
-    assert res.returncode == 0, f"clang failed:\n{res.stderr}"
-
-    res = subprocess.run([output_file], capture_output=True, text=True)
-    llvm_exec_out = res.stdout
-    llvm_exec_err = res.stderr
-    return llvm_ir, llvm_exec_out, llvm_exec_err
 
 
 if __name__ == "__main__":
